683b88eeec9c546da4fae9e3d983f4cbbcff7ce8
doplusplus <doraneplus@gmail.com>
2020-04-13 15:38:34 +0200

    rate anime: - check all and send button moved down - characteristics in v-for now - checkboxes mechanism implemented

--- webDisplay/static/webDisplay/rateAnimes.js
+++ webDisplay/static/webDisplay/rateAnimes.js
@@ -9,17 +9,37 @@ var rateAnimesComponent = function(HTMLTemplate) {
             return {
                 statsSent: false,
                 allTicked: false,
+                characteritics: ["Visuals", "Audio", "Violence", "Sexy M", "Sexy F", "Violence", "Sexy M", "Sexy F"],
+                charactCheckboxes: [false, false, true, false, false, false, false, false],
+                checkboxChanged: false,
+                allTickedChanged: false,
             }
         },
-        methods: {
-            onAllTickedChanged: function(allRequested) {
-                if (allRequested) {
-                    //check all checkboxes
-                } else {
-                    //uncheck all checkboxes
+        watch: {
+            allTicked: function(allRequested) {
+                if (this.checkboxChanged) {
+                    this.checkboxChanged = false;
+                    return;
                 }
+                this.allTickedChanged = true;
+                this.charactCheckboxes = allRequested ? [true, true, true, true, true, true, true, true] : [false, false, false, false, false, false, false, false];
             },
 
+            charactCheckboxes: function(value) {
+                if (this.allTickedChanged) {
+                    this.allTickedChanged = false;
+                    return;
+                }
+                for (let i = 0; i < value.length; i++) {
+                    if (value[i] == false && this.allTicked) {
+                        this.checkboxChanged = true;
+                        this.allTicked = false;
+                        break;
+                    }
+                }
+            },
+        },
+        methods: {
             onCheckboxClicked: function(ticked) {
                 if (allTicked && !ticked) { allTicked = false; }
             }
