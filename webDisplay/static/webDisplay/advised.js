e90c0cc6025eeaa22b331e8547d2745d6ce270a5
doplusplus <doraneplus@gmail.com>
2020-04-16 22:15:02 +0200

    advised: - deactivate search button when no tick - update button tooltip accordingly

--- webDisplay/static/webDisplay/advised.js
+++ webDisplay/static/webDisplay/advised.js
@@ -1,11 +1,23 @@
+'use strict';
+
 var adviseComponent = function(HTMLTemplate) {
 
     return {
         delimiters: ['[[', ']]'],
         data: function() {
             return {
+                bySimilarities: true,
+                byCharateristics: true,
 
             }
+        },
+        computed: {
+            filteringMethodTicked: function() {
+                return this.bySimilarities || this.byCharateristics;
+            },
+            searchButtonTitle: function() {
+                return this.filteringMethodTicked ? "Search for animes" : "Can't search because no filtering method selected"
+            },
         },
         methods: {},
         mounted: function() {},
