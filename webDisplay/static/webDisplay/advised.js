76cdfccc24c1e3f497845ff763477ae81c913850
doplusplus <doraneplus@gmail.com>
2020-04-16 23:29:01 +0200

    advised: - implemented name search - implemented anime listing
    rating:
    - cleared useless function

--- webDisplay/static/webDisplay/advised.js
+++ webDisplay/static/webDisplay/advised.js
@@ -9,7 +9,30 @@ var adviseComponent = function(HTMLTemplate) {
                 bySimilarities: true,
                 byCharateristics: true,
 
+                animeName: null,
+                animeList: [],
+                searchList: [],
+
             }
+        },
+        watch: {
+            //TODO : regroup with the similar function in rate anime and place it in rankings.js
+            animeName: function(value) {
+                this.searchList = [];
+                var isalphaNum = universalCharRegex.test(value);
+
+                if (isalphaNum) {
+                    axios.get('ranking/search/' + value)
+                        .then(response => {
+                            // when selecting an anime, a search is automically launched because the name changed
+                            if (response.data.length == 1 && response.data[0].name == this.animeName) {
+                                return;
+                            }
+                            response.data.forEach(obj => { this.searchList.push(obj.name); });
+                        })
+                        .catch(error => { console.log(error); });
+                }
+            },
         },
         computed: {
             filteringMethodTicked: function() {
@@ -19,7 +42,14 @@ var adviseComponent = function(HTMLTemplate) {
                 return this.filteringMethodTicked ? "Search for animes" : "Can't search because no filtering method selected"
             },
         },
-        methods: {},
+        methods: {
+            addAnime: function(name) {
+                this.animeList.push(name);
+            },
+            clearAll: function() {
+                this.animeList = [];
+            },
+        },
         mounted: function() {},
         template: HTMLTemplate
     };
