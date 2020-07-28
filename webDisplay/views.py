78dbd1817371b15aa9a3e7a1aa55f40cf0108d6d
doplusplus <doraneplus@gmail.com>
2020-07-28 20:18:04 +0200

    Displayes basic mobile home page

--- webDisplay/views.py
+++ webDisplay/views.py
@@ -4,11 +4,31 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth import authenticate
 
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+import re
+
+class DeviceIdentifier:
+
+    @staticmethod
+    def mobile(request):
+        """Return True if the request comes from a mobile device."""
+
+        MOBILE_AGENT_RE = re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
+
+        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+            return True
+        else:
+            return False
+
+
 
 class IndexView(generic.TemplateView):
 
+    @xframe_options_exempt
     @method_decorator(ensure_csrf_cookie)
     def get(self, request, *args, **kwargs):
+
         about   = 'About'
         ranking = 'Ranking'
         account = 'Account'
@@ -17,11 +37,11 @@ class IndexView(generic.TemplateView):
         shop    = 'Shop'
         home    = 'Home'
 
-        return render(
-            request,
-            'webDisplay/Index.html', 
-            {'about': about, 'ranking':ranking, 'account':account, 'rate':rate , 'advised':advised , 'shop':shop , 'home':home })
-
+        isMobile = DeviceIdentifier.mobile(request)
+        
+        return render( request,
+                    'webDisplay/IndexMobile.html' if isMobile else 'webDisplay/Index.html' , 
+                    {'about': about, 'ranking':ranking, 'account':account, 'rate':rate , 'advised':advised , 'shop':shop , 'home':home }) 
 
 class RankingView(generic.TemplateView):
 
